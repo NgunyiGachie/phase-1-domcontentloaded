d5d105a2b364fdc9f17f19046092e7303c048576
NgunyiGachie <antogachie@gmail.com>
2024-04-02 11:38:03 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+function changeParagraphText() {
+    const paragraph = document.querySelector('p');
+    if (paragraph) {
+        paragraph.textContent = 'This is really cool!';
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.';
+    document.body.appendChild(paragraph);
+});
+
+
+document.addEventListener("DOMContentLoaded", changeParagraphText);
+
